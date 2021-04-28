eb52660d8a841a3567afeb0b28e27d53052e4926
daniel-mita <mitadanyel@yahoo.com>
2021-04-28 18:40:13 +0300

    BC-40 #Book card like display

--- front/src/components/scroll-list.tsx
+++ front/src/components/scroll-list.tsx
@@ -1,7 +1,10 @@
-import { Grid, Icon, IconButton } from '@material-ui/core';
+import classes from '*.module.css';
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, Icon, IconButton } from '@material-ui/core';
 import { makeStyles, Paper, Typography } from '@material-ui/core';
 import React, { useState } from 'react'
 import { useData } from '../hooks/useData';
+import { Book } from '../models/book';
+import { urls, useRouting } from '../routing/routes';
 import { deleteBook, getAllBooks } from '../services/book-service';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,7 +30,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+const DisplayBook = (p: { book: Book , setReload: React.Dispatch<React.SetStateAction<boolean>>, reload: boolean}) => {
 
+    const { routeTo } = useRouting();
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.cardRoot}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="400"
+                        image="https://picsum.photos/200/400"
+                    />
+                    <CardContent>
+                        <Typography variant="h4" >{p.book.title}</Typography>
+                        <Typography style={{ textAlign: "end" }}>{p.book.author}</Typography>
+                        <Typography style={{ textAlign: "start" }}>{p.book.genre}</Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card >
+        </Grid>
+    );
+}
 
 export const ScrollList = () => {
 
@@ -43,7 +69,7 @@ export const ScrollList = () => {
         <div className={classes.paper}>
             <Grid container spacing={3}>
                 {data?.map((book) => <div key={book._id} style={{ padding: "50px" }}>
-                   <p>Test</p>
+                    <DisplayBook book={book} setReload={setReload}  reload={reload}/>
                 </div>
                 )}
             </Grid>
